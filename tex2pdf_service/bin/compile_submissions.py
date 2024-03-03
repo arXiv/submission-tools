9191a41849e9c52e88e33eeef5f9de986d8c4505
Naoyuki Tai <ntai@arxiv.org>
2024-03-03 10:50:04 -0500

    Add an option to skip updating success entries.

--- tex2pdf_service/bin/compile_submissions.py
+++ tex2pdf_service/bin/compile_submissions.py
@@ -1,5 +1,7 @@
 import os
 import time
+from sqlite3 import Connection
+
 import click
 import requests
 import logging
@@ -13,18 +15,19 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s: %(me
 
 thread_local = threading.local()
 
-def score_db(score_path: str):
+def score_db(score_path: str) -> Connection:
     """Open scorecard database"""
     db = sqlite3.connect(score_path)
     db.execute("create table if not exists score (source varcha primary key, outcome varchar, pdf varchar, status int, success bool)")
     return db
 
 @click.group()
-def cli():
+def cli() -> None:
     pass
 
 
-def tarball_to_outcome_path(tarball):
+def tarball_to_outcome_path(tarball: str) -> str:
+    """Map tarball to outcome file path"""
     parent_dir, filename = os.path.split(tarball)
     stem = filename[:-7]
     return os.path.join(parent_dir, "outcomes", "outcome-" + stem + ".tar.gz")
@@ -34,7 +37,7 @@ def tarball_to_outcome_path(tarball):
 @click.argument('submissions', nargs=1)
 @click.option('--service',  default="http://localhost:6301/convert/")
 @click.option('--score',  default="score.db", help="Score card db path")
-def compile(submissions: str, service: str, score: str):
+def compile(submissions: str, service: str, score: str) -> None:
     """Compile submissions in a directory"""
 
     def submit_tarball(tarball: str) -> None:
@@ -65,8 +68,9 @@ def compile(submissions: str, service: str, score: str):
                                 meta_file = [name for name in tar.getnames() if name.startswith("outcome-") and name.endswith(".json")]
                                 if meta_file:
                                     # Extract the specified file
-                                    meta_contents: bytes = tar.extractfile(meta_file[0])
-                                    meta = json.load(meta_contents)
+                                    meta_contents = tar.extractfile(meta_file[0])
+                                    if meta_contents:
+                                        meta = json.load(meta_contents)
                 except Exception as exc:
                     logging.warning("%s: %s", service, str(exc))
                 break
@@ -85,7 +89,8 @@ def compile(submissions: str, service: str, score: str):
 @cli.command("harvest")
 @click.argument('submissions', nargs=1)
 @click.option('--score',  default="score.db", help="Score card db path")
-def register_outcomes(submissions: str, score: str) -> None:
+@click.option('--update',  default=False, help="Update scores")
+def register_outcomes(submissions: str, score: str, update: bool) -> None:
     """Register the outcomes to a score card db"""
     submissions = os.path.expanduser(submissions)
     sdb = score_db(score)
@@ -96,8 +101,12 @@ def register_outcomes(submissions: str, score: str) -> None:
 
     tarballs = [tgz for tgz in os.listdir(submissions) if tgz.endswith(".tar.gz")]
     logging.info("Got %d tarballs", len(tarballs))
+    skipped = 0
+    updated = 0
+    good = 0
     for tarball in tarballs:
-        outcome_file = tarball_to_outcome_path(os.path.join(submissions, tarball))
+        tarball_path = os.path.join(submissions, tarball)
+        outcome_file = tarball_to_outcome_path(tarball_path)
         meta = {"status": "fail"}
         pdf_file = None
         if os.path.exists(outcome_file):
@@ -107,23 +116,36 @@ def register_outcomes(submissions: str, score: str) -> None:
                                  name.startswith("outcome-") and name.endswith(".json")]
                     if meta_file:
                         # Extract the specified file
-                        meta_contents: bytes = tar.extractfile(meta_file[0])
-                        meta = json.load(meta_contents)
+                        meta_contents = tar.extractfile(meta_file[0])
+                        if meta_contents:
+                            meta = json.load(meta_contents)
                     pdf_file = meta.get("pdf_file")
-                    if pdf_file:
-                        tar_pdf_path = meta["out_directory"] + "/" + pdf_file
-                        pdf_contents: bytes = tar.extractfile(tar_pdf_path)
+                    # if pdf_file:
+                        # tar_pdf_path = meta["out_directory"] + "/" + pdf_file
+                        # pdf_contents = tar.extractfile(tar_pdf_path)
 
             except Exception as exc:
                 logging.warning("%s: %s - deleting outcome", outcome_file, str(exc))
                 os.unlink(outcome_file)
         success = meta.get("status") == "success"
-        key = os.path.join(submissions, tarball)
+        # If the success is already reported, no need to update
+        cursor = sdb.cursor()
+        cursor.execute("select count(*) from outcomes where source = ? and success", tarball_path)
+        count = cursor.fetchone()[0]
+        cursor.close()
+        if success and count > 0 and not update:
+            skipped += 1
+            continue
+        # Upsert the result
         cursor = sdb.cursor()
         cursor.execute("begin")
-        cursor.execute("insert into score (source, outcome, pdf, success) values (?, ?, ?, ?) on conflict(source) do update set outcome=excluded.outcome, pdf=excluded.pdf, success=excluded.success", (key, json.dumps(meta, indent=2), pdf_file, success))
+        cursor.execute("insert into score (source, outcome, pdf, success) values (?, ?, ?, ?) on conflict(source) do update set outcome=excluded.outcome, pdf=excluded.pdf, success=excluded.success", (tarball_path, json.dumps(meta, indent=2), pdf_file, success))
         cursor.execute("commit")
         cursor.close()
+        updated += 1
+        if success:
+            good += 1
+    logging.info("Total: %d, skipped: %d, updated: %d, good: %d, bad: %d", len(tarballs), skipped, updated, good, len(tarballs) - good)
 
 
 if __name__ == '__main__':
