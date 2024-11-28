341b7eabd6288dfa80f88090d79d579801b7f73c
Norbert Preining <norbert@arxiv.org>
2024-11-28 13:12:58 +0900

    Allow ruff-ing conftest.py

--- conftest.py
+++ conftest.py
@@ -1,4 +1,8 @@
+"""Pytest configuration file."""
+
+
 def pytest_addoption(parser):
+    """Add command line options to pytest."""
     parser.addoption("--keep-docker-running", action="store_true", help="keep docker image running")
     parser.addoption("--no-docker-setup", action="store_true", help="do not run docker setup, expect a running docker")
     parser.addoption("--docker-port", action="store", default="33031", help="outside docker port to use")
