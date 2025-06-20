f8af07560de5e81e5fb9ce0ea45de6224281d40e
Norbert Preining <norbert@arxiv.org>
2025-06-20 23:44:45 +0200

    watermark: detect transparent/uni-color/multi-color background and stamp accordingly

--- tex2pdf-service/tex2pdf/pdf_watermark.py
+++ tex2pdf-service/tex2pdf/pdf_watermark.py
@@ -57,6 +57,42 @@ def add_watermark_text_to_pdf(watermark: Watermark, in_pdf: pathlib.Path | str, 
             rig = 32
             top = (page_size[1] - wm_length) / 2
             bot = (page_size[1] + wm_length) / 2
+            # let us check for page color as well as overlap
+            pix = page.get_pixmap(clip=pymupdf.Rect(lef, top, rig, bot), alpha=True)
+            color_count = pix.color_count()
+            color_topusage = pix.color_topusage()
+            # cases:
+            # 1. transparent page, no contents --> color_topusage == (1.0, b'\x00\x00\x00\x00')
+            # 2. transparent page, with contents -->
+            #    depending on the amount of contents and possible images, but with normal text
+            #    we would still get
+            #        color_topusage == (0.NNNNNNNNNNN, b'\x00\x00\x00\xff')
+            #    with 0.NNNNNNNNNNN being >= 0.5 the percentage of transparent in the box
+            # 3. non-transparent page, no contents --> color_topusage == (1.0,b'\xff\xff\xff\xff')
+            #    (in case of white background, some other byte string for another color)
+            # 4. non-transparent page, with contents -->
+            #    as above but
+            #        color_topusage == (0.NNNNNNNNNNN, b'\xff\xff\xff\xff')
+            if color_count == 1:
+                # if we have only one color in the rect, it is either transparent or some
+                # background color, but not real information
+                # TODO: for real overlay detection, we need to adjust this
+                overlay = True
+                if color_topusage == (1.0, b"\x00\x00\x00\x00"):
+                    # transparent background, so we can overlay
+                    no_overlap = True
+                else:
+                    # some uni-color background, maybe from a logo or background design
+                    no_overlap = False
+            else:
+                overlay = False
+                no_overlap = False
+            logger.debug("Color count in watermark area: %d", color_count)
+            logger.debug("Color top usage in watermark area: %s", color_topusage)
+            logger.debug("Detected no_overlap: %s", no_overlap)
+            # this should fix most cases:
+            # - transparent or uni-color background get an overlay on top of it
+            # - everything else gets an "underlay" (i.e. the watermark is below the content)
             page.insert_text(
                 pymupdf.Point(rig, bot),
                 watermark.text,
@@ -64,7 +100,7 @@ def add_watermark_text_to_pdf(watermark: Watermark, in_pdf: pathlib.Path | str, 
                 fontsize=fsize,
                 rotate=90,
                 color=(0.5, 0.5, 0.5),
-                overlay=False,
+                overlay=overlay,
             )
             if watermark.link:
                 page.insert_link(
